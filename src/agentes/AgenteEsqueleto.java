e990e977c6130c1a735a4eb56fbf061f25156ac3
xenahort <juan.carlos.wow.95@gmail.com>
2017-02-11 18:03:36 +0100

    -AgenteEsqueleto registrado en las paginas amarillas

--- src/agentes/AgenteEsqueleto.java
+++ src/agentes/AgenteEsqueleto.java
@@ -7,6 +7,10 @@ package agentes;
 
 import GUI.AgenteDemoJFrame;
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 
 /**
  *
@@ -14,41 +18,58 @@ import jade.core.Agent;
  * agentes
  */
 public class AgenteEsqueleto extends Agent {
+
     //Variables del agente
     private AgenteDemoJFrame myGui;
 
     @Override
     protected void setup() {
-       //Inicialización de las variables del agente
-       
-       //Configuración del GUI
-       myGui = new AgenteDemoJFrame(this);
-       myGui.setVisible(true);
-       
-       //Registro del agente en las Páginas Amarrillas
-       
-       //Registro de la Ontología
-       
-       System.out.println("Se inicia la ejecución del agente: " + this.getName());
-       myGui.presentarSalida("Se inicia la ejecución del agente: " + this.getName()+"\n");
-       
-       //Añadir las tareas principales
+        
+        //Inicialización de las variables del agente
+
+        //Configuración del GUI
+        myGui = new AgenteDemoJFrame(this);
+        myGui.setVisible(true);
+
+        //Registro del agente en las Páginas Amarrillas
+        myGui.presentarSalida("Agente registrado en las paginas amarillas\n");
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("Tipo de Servicio");
+        sd.setName("Nombre del Servicio");
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+
+        //Registro de la Ontología
+        
+        
+        System.out.println("Se inicia la ejecución del agente: " + this.getName());
+
+        
+        //Añadir las tareas principales
     }
 
     @Override
     protected void takeDown() {
-       //Desregristo del agente de las Páginas Amarillas
-       
-       //Liberación de recursos, incluido el GUI
-       myGui.dispose();
-       
-       //Despedida
-       System.out.println("Finaliza la ejecución del agente: " + this.getName());
+        //Desregristo del agente de las Páginas Amarillas
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+
+        //Liberación de recursos, incluido el GUI
+        myGui.dispose();
+
+        //Despedida
+        System.out.println("Finaliza la ejecución del agente: " + this.getName());
     }
-    
+
     //Métodos de trabajo del agente
-    
-    
     //Clases internas que representan las tareas del agente
-      
 }
