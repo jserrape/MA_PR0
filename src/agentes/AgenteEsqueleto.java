552eaa91dd9690139a7470f70be679c68bc4e220
xenahort <juan.carlos.wow.95@gmail.com>
2017-02-12 14:34:03 +0100

    -La nueva tarea termina definitivamente a las 10 ejecuciones.

--- src/agentes/AgenteEsqueleto.java
+++ src/agentes/AgenteEsqueleto.java
@@ -12,6 +12,8 @@ import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -22,14 +24,15 @@ public class AgenteEsqueleto extends Agent {
 
     //Variables del agente
     private AgenteDemoJFrame myGui;
-    int ejecuciones;
-    
+    int ejecuciones, ejecuciones2;
+    TareaEjercicio tareaE;
+
     @Override
     protected void setup() {
-        
+
         //Inicialización de las variables del agente
-        ejecuciones=0;
-        
+        ejecuciones2 = ejecuciones = 0;
+
         //Configuración del GUI
         myGui = new AgenteDemoJFrame(this);
         myGui.setVisible(true);
@@ -49,13 +52,12 @@ public class AgenteEsqueleto extends Agent {
         }
 
         //Registro de la Ontología
-        
-        
         System.out.println("Se inicia la ejecución del agente: " + this.getName());
 
-        
         //Añadir las tareas principales
         addBehaviour(new TareaEjemplo(this, 10000));
+        tareaE=new TareaEjercicio(this, 10000);
+        addBehaviour(tareaE);
         myGui.habilitarCerrado(false);
     }
 
@@ -78,20 +80,48 @@ public class AgenteEsqueleto extends Agent {
     //Métodos de trabajo del agente
     //Clases internas que representan las tareas del agente
     public class TareaEjemplo extends TickerBehaviour {
+
         //Tarea de ejemplo que se repite cada 10 segundos
         public TareaEjemplo(Agent a, long period) {
             super(a, period);
+            myGui.presentarSalida("\nComienza la tarea TareaEjemplo");
         }
 
         @Override
         protected void onTick() {
-           ejecuciones++;
-           myGui.presentarSalida("\nEjecución número: " + ejecuciones);
-           if(ejecuciones==4){
-               myGui.habilitarCerrado(true);
-               myGui.presentarSalida("\nYa se puede cerrar la interfaz.");
-           } 
+            ++ejecuciones;
+            myGui.presentarSalida("\nEjecución número: " + ejecuciones + " de TareaEjemplo");
+            if (ejecuciones == 4) {
+                myGui.habilitarCerrado(true);
+                myGui.presentarSalida("\nYa se puede cerrar la interfaz.");
+            }
+        }
+    }
+
+    public class TareaEjercicio extends TickerBehaviour {
+
+        //Tarea de ejemplo que se repite cada 10 segundos
+        public TareaEjercicio(Agent a, long period) {
+            super(a, period);
+            myGui.presentarSalida("\nComienza la tarea TareaEjercicio");
+        }
+
+        @Override
+        protected void onTick() {
+            ++ejecuciones2;
+            myGui.presentarSalida("\nEjecución número: " + ejecuciones2 + " de TareaEjercicio");
+            if(ejecuciones2==10){
+                myGui.presentarSalida("\nAcaba la tarea: TareaEjercicio");
+                removeBehaviour(tareaE);
+            }
         }
         
+        public void secuenciaPar(){
+        
+        }
+
+        public void secuenciaImpar(){
+        
+        }
     }
 }
